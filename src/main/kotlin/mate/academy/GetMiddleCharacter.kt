c3558f75d2907ee44569fa9daa37f6c761605812
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-02-25 19:20:04 +0200

    Changed program structure.

--- src/main/kotlin/mate/academy/GetMiddleCharacter.kt
+++ src/main/kotlin/mate/academy/GetMiddleCharacter.kt
@@ -4,8 +4,15 @@ package mate.academy
 // and returns middle character (or characters) as a string
 
 fun getMiddleString(originalString: String): String {
-    if (originalString.length % 2 == 0 && originalString != "") {
-        return originalString.substring(originalString.length / 2 - 1, originalString.length / 2 + 1)
+    var result: String = ""
+    if (originalString == "") {
+        return result
     }
-    return originalString[originalString.length / 2].toString()
+
+    if (originalString.length % 2 == 0) {
+        result = originalString.substring(originalString.length / 2 - 1, originalString.length / 2 + 1)
+    } else {
+        result = originalString[originalString.length / 2].toString()
+    }
+    return result
 }
