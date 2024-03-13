0f975ed312ba8ddef0d4613698ec1182fff3e077
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2024-03-13 16:28:02 +0200

    second version

--- src/main/kotlin/mate/academy/GetMiddleCharacter.kt
+++ src/main/kotlin/mate/academy/GetMiddleCharacter.kt
@@ -2,9 +2,11 @@ package mate.academy
 
 fun getMiddleString(originalString: String): String {
     if (originalString.isEmpty()) return ""
-    if (originalString.length % 2 != 0) {
-        return originalString.substring((originalString.length / 2), (originalString.length / 2) + 1)
+    val answer: String = if (originalString.length % 2 != 0) {
+        originalString.substring((originalString.length / 2), (originalString.length / 2) + 1)
+    } else {
+        originalString.substring((originalString.length / 2) - 1, (originalString.length / 2) + 1)
     }
-    return originalString.substring((originalString.length / 2) -1 , (originalString.length / 2) + 1)
+    return answer
 }
 
