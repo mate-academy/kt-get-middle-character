7058176792e9ac2513ab6cf1b13e09ddf377fb60
oleksii.b <oleksii.b@dev.tagsoft.co>
2025-01-16 12:13:16 +0200

    resolved task

--- src/main/kotlin/mate/academy/GetMiddleCharacter.kt
+++ src/main/kotlin/mate/academy/GetMiddleCharacter.kt
@@ -4,5 +4,11 @@ package mate.academy
 // and returns middle character (or characters) as a string
 
 fun getMiddleString(originalString: String): String {
-    return ""
+     return if (originalString.isEmpty()) {
+         ""
+     } else if (originalString.length % 2 == 0) {
+        originalString.substring(originalString.length / 2 - 1, originalString.length / 2 + 1)
+    } else {
+        originalString.substring(originalString.length / 2, originalString.length / 2 + 1)
+     }
 }
