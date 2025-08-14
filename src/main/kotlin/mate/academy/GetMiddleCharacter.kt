614dea873b1d43420256eccc055148aa5f280f1f
Dmytro Sokolovskyi <dm.sokolovskyi@gmail.com>
2025-08-14 00:23:54 -0400

    added solution getMiddleString func

--- src/main/kotlin/mate/academy/GetMiddleCharacter.kt
+++ src/main/kotlin/mate/academy/GetMiddleCharacter.kt
@@ -1,8 +1,13 @@
 package mate.academy
 
-// implement the `getMiddleString()` function, which takes a string
-// and returns middle character (or characters) as a string
-
 fun getMiddleString(originalString: String): String {
-    return ""
+    if (originalString.isEmpty()) return ""
+
+    val mid = originalString.length / 2
+
+    return if (originalString.length % 2 == 0) {
+        originalString.substring(mid - 1, mid + 1)
+    } else {
+        originalString.substring(mid, mid + 1)
+    }
 }
